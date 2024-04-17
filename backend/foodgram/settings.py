bb66bd6cb5069c98361c390a6b16bb4dacb889c9
potesuch <potesuch1@yandex.ru>
2024-04-17 09:42:38 +0300

    minor fixes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.87.1']
 
